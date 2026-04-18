2ffd5d50cfc1c5f87ddc60d895a265787bf3c3a7
otto-kirchheim <janotto1989@gmail.com>
2026-04-18 20:28:55 +0200

    remove unused EWT and Neben utility functions

--- src/ts/Neben/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import getNebengeldDaten from './getNebengeldDaten';
-import addNebengeldTag from './addNebengeldTag';
-import persistNebengeldTableData from './persistNebengeldTableData';
-
-export { addNebengeldTag, getNebengeldDaten, persistNebengeldTableData };
-export { default as syncNebengeldTimesFromEwtRows } from '../../orchestration/syncEwtToNeben';
